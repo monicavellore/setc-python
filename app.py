bfb66215332b520e13dfa2b9906039e73a42515a
monicavaradhan <monica.2024@vitstudent.ac.in>
2026-09-12 19:03:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+def calculate_total():
+    a = 10
+    b = 20
+    total = a + b
+    print("First number:", a)
+    print("Second number:", b)
+    print("Total:", total)
+
+
+if __name__ == "__main__":
+    calculate_total()
